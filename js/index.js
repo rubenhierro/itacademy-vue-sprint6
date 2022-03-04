e7a861cc55c114f66b86c47c4dfb9cad85122ab3
rubenhierro <rubenhierro@gmail.com>
2022-03-04 18:38:20 +0100

    ex1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-Vue.component("home", {});
+Vue.component("home", {
+  template: `
+    <escena></escena>
+    `,
+});
 Vue.component("escena", {
   template: `
   <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>
